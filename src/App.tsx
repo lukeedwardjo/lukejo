bd02692684b770a9ac547586cf0174880a49e160
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2022-02-19 16:09:35 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="grid place-items-center h-screen">
+      <div>
+        <div>Luke Johnson</div>
+        <ul>
+          <li>
+            <a href="https://twitter.com/lukeedwardjo">Twitter</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
