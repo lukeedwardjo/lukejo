0454fcd81296040dfce83afd476a6ff68edb1f5f
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2022-02-22 13:44:51 -0600

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className=" max-w-md bg-gradient-to-r from-indigo-500 ml-10">test</div>
+    <>
+      <span className="w-auto">test</span>
+      <span className="max-w-md h-vh bg-gradient-to-r from-indigo-500 m-auto">
+        content
+      </span>
+      <span className="w-auto">test</span>
+    </>
   );
 }
 
