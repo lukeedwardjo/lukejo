f8fbd0c881b874a209353f2ef0d4a95fc20834b4
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2022-02-22 19:04:57 -0600

    update text size

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="flex">
       <div className="flex-auto"></div>
-      <div className=" text-2xl font-serif selection:bg-lew-blue selection:text-lew-yellow h-screen w-64 shrink-0">
-        <div className="text-lew-blue">Luke Johnson</div>
+      <div className="font-serif selection:bg-lew-blue selection:text-lew-yellow h-screen w-64 shrink-0">
+        <div className="text-lew-blue text-2xl">Luke Johnson</div>
         <ul className="list-disc text-black">
           <li>
             <a href="https://twitter.com/lukeedwardjo">follow me on twitter</a>
