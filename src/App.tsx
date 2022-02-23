f61a422c28cf3f19fc68cdca5cdfd24bba0e02ec
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2022-02-22 19:26:33 -0600

    style tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex items-center justify-center">
       <div className="flex-auto"></div>
-      <div className="font-serif selection:bg-lew-blue selection:text-lew-yellow h-screen w-64 shrink-0 mt-6">
+      <div className="font-serif selection:bg-lew-blue selection:text-lew-yellow h-screen w-64 shrink-0 mt-24">
         <div className="text-lew-blue text-5xl flex items-center justify-center font-bold">
           Luke&nbsp;Johnson
         </div>
