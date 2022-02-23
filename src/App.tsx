77d986604825a39921211220099779a3518f2757
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2022-02-22 19:13:22 -0600

    center finally

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex">
+    <div className="flex items-center justify-center">
       <div className="flex-auto"></div>
       <div className="font-serif selection:bg-lew-blue selection:text-lew-yellow h-screen w-64 shrink-0">
-        <div className="text-lew-blue text-2xl">Luke Johnson</div>
-        <ul className="list-disc text-black">
+        <div className="text-lew-blue text-2xl flex items-center justify-center">
+          Luke Johnson
+        </div>
+        <ul className="list-disc text-black flex items-center justify-center">
           <li>
             <a href="https://twitter.com/lukeedwardjo">follow me on twitter</a>
           </li>
