f2ee73386898f2ffa3d9c635dfe6b5d7065cdd7b
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2022-02-22 19:02:53 -0600

    add twitter handlw

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ function App() {
   return (
     <div className="flex">
       <div className="flex-auto"></div>
-      <div className="text-lew-blue text-2xl font-serif selection:bg-lew-blue selection:text-lew-yellow h-screen w-64 shrink-0">
-        Luke Johnson
+      <div className=" text-2xl font-serif selection:bg-lew-blue selection:text-lew-yellow h-screen w-64 shrink-0">
+        <div className="text-lew-blue">Luke Johnson</div>
+        <ul className="list-disc text-black">
+          <li>
+            <a href="https://twitter.com/lukeedwardjo">follow me on twitter</a>
+          </li>
+        </ul>
       </div>
       <div className="flex-auto"></div>
     </div>
