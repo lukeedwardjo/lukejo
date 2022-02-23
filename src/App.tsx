88c2887a2ec2487ca9d0564ef9006d75e2ff75b8
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2022-02-22 19:23:50 -0600

    add margin

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ function App() {
   return (
     <div className="flex items-center justify-center">
       <div className="flex-auto"></div>
-      <div className="font-serif selection:bg-lew-blue selection:text-lew-yellow h-screen w-64 shrink-0">
+      <div className="font-serif selection:bg-lew-blue selection:text-lew-yellow h-screen w-64 shrink-0 mt-6">
         <div className="text-lew-blue text-5xl flex items-center justify-center font-bold">
           Luke&nbsp;Johnson
         </div>
-        <ul className="list-disc text-black flex items-center justify-center">
+        <ul className="list-disc text-black flex items-center justify-center mt-6">
           <li>
             <a href="https://twitter.com/lukeedwardjo">follow me on twitter</a>
           </li>
