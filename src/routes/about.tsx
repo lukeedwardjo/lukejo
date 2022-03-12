6bf9046a8643593e37d48de426ecbd843792288e
Luke Johnson <35469814+lukeedwardjo@users.noreply.github.com>
2022-03-11 18:01:54 -0600

    update about

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,8 +1,9 @@
 export default function About() {
   return (
     <div className="flex items-center justify-center mt-6 ml-6 mr-6">
-      Hey, my name is Luke Johnson. I'm a software engineer who lives in the
-      best city in the midwest, Springfield, Missouri.
+      Hi, I’m Luke Johnson. I’m a software engineer who lives in Springfield, Missouri. I build functional, maintainable web applications that bring tangible value to end users. Right now I work in the security industry at Digital Monitoring Products where I build applications that connect our customers to their security system for arming/disarming, viewing security cameras, and home automation including lights and locks.
+
+The technologies I’m working with right now are React, GraphQL, and Relay. I’m interested in technologies that build the infrastructure of the cloud like Go, Kubernetes, and tools built by HashiCorp. 
     </div>
   );
 }
