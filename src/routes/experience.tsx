1977116c40c0c2f8cf13136064e64ffa60b7935e
Luke Johnson <35469814+lukeedwardjo@users.noreply.github.com>
2022-07-07 10:28:35 -0500

    Update experience.tsx

--- src/routes/experience.tsx
+++ src/routes/experience.tsx
@@ -1,7 +1,7 @@
 export default function Experience() {
   return (
     <div className="flex items-center justify-center mt-6 ml-6 mr-6">
-      <span>
+      <p>
         I became familiar with Node.js in college but gained most of my
         experience with it working with Apollo Server at my current workplace.
         I've written dozens of features built on top of our GraphQL API since I
@@ -9,8 +9,8 @@ export default function Experience() {
         When starting on this team the GraphQL schema and its resolvers lived in
         the client, but we have since separated our GraphQL onto its own
         servers.
-      </span>
-      <span>
+      </p>
+      <p>
         Our implementation of GraphQL includes TypeScript, Apollo Server, and
         GraphQL Code Generator (to translate GraphQL types to TypeScript types).
         We use GraphQL because we have many different APIs built with .NET Web
@@ -18,8 +18,8 @@ export default function Experience() {
         predicable behavior. Our only data sources for GraphQL are these REST
         APIs. We use Relay (Facebook's GraphQL client) for state management in
         our web applications.
-      </span>
-      <span>
+      </p>
+      <p>
         My workflow when writing a new feature in GraphQL begins with evaluating
         what new data the client will need how that data fits into GraphQL
         schema. For new data, I create TypeScript types of what we expect from
@@ -28,12 +28,12 @@ export default function Experience() {
         file. I then write the resolvers using the methods that fetch the data,
         and because we use GraphQL code Generator we get a lot of help from
         TypeScript when writing the resolvers.
-      </span>
-      <span>
+      </p>
+      <p>
         Overall, I think GraphQL is a very useful tool, especially for
         abstracting the data element and business logic away from the clients
         which only serves to make them more maintainable and nimbler.
-      </span>
+      </p>
     </div>
   );
 }
